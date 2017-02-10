65b439df0a31e42cb50187b6c21b3dedfc61ea23
glennon <glennon@gmail.com>
2017-02-09 22:52:58 -0800

    use for loops

--- bubbles.js
+++ bubbles.js
@@ -1,45 +1,69 @@
 // initialize variables
 var scores = [60,50,60,58,54,54,58,50,52,54,48,69,34,55,51,52,44,51,69,64,66,55,52,61,46,31,57,52,44,18,41,53,55,61,51,44];
+var costs = [0.25,0.27,0.25,0.25,0.25,0.25,
+             0.33,0.31,0.25,0.29,0.27,0.22,
+             0.31,0.25,0.25,0.33,0.21,0.25,
+             0.25,0.25,0.28,0.25,0.24,0.22,
+             0.20,0.25,0.30,0.25,0.24,0.25,
+             0.25,0.25,0.27,0.25,0.26,0.29];
 var output;
-var i = 0;
-var x = 0;
-var y = 0;
-var highestValue;
-var highestAddress = [];
+
 
 // output bubble experiment values
-while (i < scores.length) {
-  output = "Bubble solution #" + i + " score: " + scores[i];
-  console.log(output);
-  i = i + 1;
+function printAndGetHighScores(scores,costs) {
+  var highestValue;
+  for (var i = 0; i < scores.length; i++) {
+    output = "Bubble solution #" + i +
+    " score: " + scores[i] + " cost: "+ costs[i];
+    console.log(output);
+    if (i == 0) {
+      highestValue = scores[i];
+    } else {
+      if (highestValue < scores[i]) {
+        highestValue = scores[i];
+      }
+    }
+  }
+  return highestValue;
 }
 
+var highScore = printAndGetHighScores(scores,costs);
 // output number of experiments
 output = "Bubble tests: " + scores.length;
 console.log(output);
 
-// output experiment # with highest bubble yield
-while (x < scores.length) {
-  if (x == 0) {
-    highestValue = scores[x];
-  } else {
-    if (highestValue < scores[x]) {
-      highestValue = scores[x];
+output = "Highest bubble score: " + highScore;
+console.log(output);
+
+function getBestResults(scores,myHighScore) {
+  // output address locations of highest bubble yield
+  var highestAddress = [];
+  for (var y = 0; y < scores.length; y++) {
+    if (scores[y] == myHighScore) {
+      // spec requires a # before the value, so added here to make output easier
+      highestAddress.push(y);
     }
   }
-  x = x + 1;
+  return highestAddress;
 }
-output = "Highest bubble score: " + highestValue;
+
+var bestSolutions = getBestResults(scores,highScore);
+output = "Solutions with highest score: " + bestSolutions;
 console.log(output);
 
-// output address locations of highest bubble yield
-while (y < scores.length) {
-  if (scores[y] == highestValue) {
-    // spec requires a # before the value, so added here to make output easier
-    highestAddress.push("#" + y);
+function getMostCostEffectiveSolution(scores, costs, highScore) {
+  var lilCost = 9999;
+  var lilCostIndex;
+  for (var q = 0; q < scores.length; q++) {
+    if (scores[q] == highScore) {
+      if (lilCost > costs[q]) {
+        lilCostIndex = q;
+        lilCost = costs[q];
+      }
+    }
   }
-  y = y + 1;
+  return lilCostIndex;
 }
 
-output = "Solutions with highest score: " + highestAddress.toString();
-console.log(output);
+var mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
+console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective.");
